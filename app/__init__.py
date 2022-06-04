8140de53dbef5ffec7ee46b9a4a466567fcb6709
Paul Moros <pmoros@outlook.com>
2022-06-04 15:50:11 -0400

    chore: db_url env variable name changed

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
