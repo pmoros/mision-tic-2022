2590e1c56f2450cf879ec7d4abbedd813aad3171
Paul Moros <pmoros@outlook.com>
2022-06-04 12:49:33 -0400

    chore(test): add product controller tests

--- app/controllers.py
+++ app/controllers.py
@@ -24,4 +24,23 @@ class UserController():
         return user
 
 
+class ProductController():
+    def get_all_products(self):
+        # return Product.query.all()
+        pass
+
+    def get_product(self, product_id):
+        # return Product.query.get(product_id)
+        pass
+
+    def get_products_ordered_by(self, field, asc=True):
+        # if asc:
+        #     return Product.query.order_by(Product.name).all()
+        # else:
+        #     return Product.query.order_by(Product.name.desc()).all()
+
+        pass
+
+
 user_controller = UserController()
+product_controller = ProductController()
