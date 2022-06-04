6c72eaec8e69d7a13fc06d5a076fbff8ff986705
Paul Moros <pmoros@outlook.com>
2022-06-04 17:56:01 -0400

    fix: order details misspelling

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class UserChoices(enum.Enum):
     VENDOR = 'VENDOR'
     CUSTOMER = 'CUSTOMER'
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sub = db.Column(db.String(255), nullable=False, unique=True)
@@ -18,6 +19,7 @@ class User(UserMixin, db.Model):
     order_customer = db.relationship("Order", backref="order", uselist=False)
     stores = db.Column(db.Integer, db.ForeignKey('store.id'), nullable=False)
     city = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
+
 
 class ProductLine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,10 +36,11 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     availabe = db.Column(db.Float, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey('store.id'), nullable=False)
-    productLine_id = db.Column(db.Integer, db.ForeignKey('product_line.id'), nullable=False)
-    orderDetalls = db.relationship("OrderDetail", backref="order_detail", uselist=False)
-    
-    
+    productLine_id = db.Column(db.Integer, db.ForeignKey(
+        'product_line.id'), nullable=False)
+    orderDetalls = db.relationship(
+        "OrderDetail", backref="order_detail", uselist=False)
+
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,6 +48,7 @@ class City(db.Model):
     population = db.Column(db.Integer, nullable=False)
     stores = db.relationship("Store", backref="store", uselist=False)
     users = db.relationship("User", backref="user", uselist=False)
+
 
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -54,6 +58,7 @@ class Store(db.Model):
     products = db.relationship("Product", backref="product", uselist=False)
     city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
 
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_date = db.Column(db.DateTime, nullable=False)
@@ -62,15 +67,20 @@ class Order(db.Model):
     comment = db.Column(db.String(255), nullable=True)
     Confirmed = db.Column(db.Boolean, default=False, nullable=False)
     accepted = db.Column(db.Boolean, default=False, nullable=False)
-    orderDetalls = db.relationship("OrderDetail", backref="order_detail", uselist=False)
+    orderDetails = db.relationship(
+        "OrderDetail", backref="order_detail", uselist=False)
     vendor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    customer_id = db.Column(
+        db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 class OrderDetail(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    order_id =  db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+
 
 def load_test_data():
     # User test data
