6fcbe295e624f21fe2c10d51d8a3331f1d4749ab
oswaldp <oswaldpale@gmail.com>
2022-06-04 12:50:55 -0500

    models product - productline

--- app/models.py
+++ app/models.py
@@ -9,6 +9,22 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     profile_pic = db.Column(db.String(255), nullable=False)
 
+class ProductLine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    products = db.relationship("Product", backref="product", uselist=False)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    stock = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    availabe = db.Column(db.Float, nullable=False)
+    store = db.Column(db.Float, nullable=False)
+    productLine_id = db.Column(db.Integer, db.ForeignKey('product_line.id'), nullable=False)
 
 def load_test_data():
     # User test data
